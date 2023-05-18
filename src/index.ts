42e7117065576c05a5fefa9861583650cc8800f8
Paula Santamaría <m.paula.santamaria@gmail.com>
2023-05-18 20:23:46 -0300

    setup canvas and controller

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import chalk from 'chalk';
+import { Canvas, Palette } from './canvas';
+import { Controller } from './controller';
+
+const palette: Palette = {
+    '1': '#f0dab1',
+    '2': '#e39aac',
+    '3': '#c45d9f',
+    '4': '#634b7d',
+    '5': '#6461c2',
+    '6': '#2ba9b4',
+    '7': '#93d4b5',
+    '8': '#f0f6e8'
+};
+
+// Draw menu.
+let menu = 'Move: ↑ ↓ ← → | Clear color: del | Exit: esc\nPalette: ';
+for (const colorKey in palette) {
+    menu += chalk.hex('#212121').bgHex(palette[colorKey])(` ${colorKey} `);
+}
+
+const canvas = new Canvas(32, 24, palette, menu);
+
+// Handle user commands.
+const controller = new Controller()
+    .on('up', () => canvas.move('up'))
+    .on('down', () => canvas.move('down'))
+    .on('left', () => canvas.move('left'))
+    .on('right', () => canvas.move('right'))
+    .on('delete', () => canvas.clear())
+    .on('escape', () => process.exit(0));
+
+for (const colorKey in palette) {
+    controller.on(colorKey, () => canvas.paint(colorKey));
+}
+
+controller.build();
+
+// Start app.
+canvas.initialize();
+canvas.render();
